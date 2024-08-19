78939502045dc117830312b1d9a36523b8af131d
lucasrodriguesx <gustavodrdr19@gmail.com>
2024-08-19 20:34:35 -0300

    fundamentos

---
+++ src/app/components/Cabecalho.js
@@ -0,0 +1,10 @@
+export default function Cabecalho(){
+    return (
+        <>
+            <div>
+                <h1>Cabecalho</h1>
+                <p>Isto e o Cabecalho da minha pagina</p>
+            </div>
+        </>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+
+import { Button } from "react-bootstrap";
+import Cabecalho from "./components/Cabecalho";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="primary">Primary</Button>{' '}
+      <Cabecalho />
+    </main>
+  );
+}
