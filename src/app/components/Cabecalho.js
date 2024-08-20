4005c7b90f2ff6119bc87e8a95cedeed520c62a6
lucasrodriguesx <gustavodrdr19@gmail.com>
2024-08-19 21:30:42 -0300

    fundamentos

--- src/app/components/Cabecalho.js
+++ src/app/components/Cabecalho.js
@@ -1,9 +1,10 @@
-export default function Cabecalho(){
+export default function Cabecalho(props){
+
     return (
         <>
             <div>
-                <h1>Cabecalho</h1>
-                <p>Isto e o Cabecalho da minha pagina</p>
+                <h1>{props.titulo}</h1>
+                <p>{props.sub}</p>
             </div>
         </>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
-
 import { Button } from "react-bootstrap";
 import Cabecalho from "./components/Cabecalho";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Button variant="primary">Primary</Button>{' '}
-      <Cabecalho />
+      <Button variant="outline-primary">Primary</Button>{' '}
+      <Cabecalho titulo="Pagina Inical" sub="Lucas"/>
+      <Link href='/fundamentos'> paginas fundamentos</Link>
+
     </main>
   );
 }
