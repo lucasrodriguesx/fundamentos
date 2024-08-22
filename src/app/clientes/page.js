42c0aaae105c2feb3cd0e8ca80e0f5eb47676909
lucasrodriguesx <gustavodrdr19@gmail.com>
2024-08-22 19:37:12 -0300

    Fundamentos

---
+++ src/app/clientes/page.js
@@ -0,0 +1,47 @@
+import { Table } from "react-bootstrap";
+import Cabecalho from "../components/Cabecalho";
+import Link from "next/link";
+
+export default function Clientes(props){
+
+    props.titulo
+    props.sub
+
+    return (
+        <>
+        <Cabecalho titulo="Clientes" sub="Página de clientes usando o cabeçalho"/>
+
+        <Link href="/">Página inicial</Link> <br />
+        <Link href="/fundamentos">Fundamentos</Link>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+    </>
+    )
+}
