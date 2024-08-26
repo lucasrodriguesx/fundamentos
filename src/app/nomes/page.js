c549a735c9835f19d8db5f934775af1193af9bef
lucasrodriguesx <gustavodrdr19@gmail.com>
2024-08-26 20:54:51 -0300

    Fundamentos

--- src/app/nomes/page.js
+++ src/app/nomes/page.js
@@ -4,9 +4,13 @@ import Pagina from "@/app/components/Pagina";
 
 export default function Nomes(){
 
+    let nome = 'Lucas'
+
     return (
         <Pagina titulo="Nomes">
-
+        <button>Primeiro Nome</button>
+        <h1>{nome}</h1>
+        <button>Primeiro Nome</button>
         </Pagina>
     );
   }
